448c224a8f747aabde4172125d50c14144973f26
Sampsukkie <117081212+Sampsukkie@users.noreply.github.com>
2023-12-21 16:16:45 +0200

    TopDownMovement

---
+++ Assets/Scripts/TopDownPlayerController.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopDownPlayerController : MonoBehaviour
+{
+    public Animator playerNator;
+
+    public float PlayerSpeed = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 moveVector = new Vector3(horizontal, vertical, 0f);
+        moveVector = moveVector.normalized;
+        transform.position += moveVector * PlayerSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            playerNator.SetBool("backward", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.S))
+        {
+            playerNator.SetBool("backward", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            playerNator.SetBool("forward", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            playerNator.SetBool("forward", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            playerNator.SetBool("goRight", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            playerNator.SetBool("goRight", false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            playerNator.SetBool("goLeft", true);
+        }
+
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            playerNator.SetBool("goLeft", false);
+        }
+    }
+}
