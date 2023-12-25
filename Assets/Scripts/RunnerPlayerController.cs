b9e160e50fdd76358d820ee10dc655d273098084
Sampsukkie <117081212+Sampsukkie@users.noreply.github.com>
2023-12-25 17:56:13 +0200

    EndlessRunnerWorlds

--- Assets/Scripts/RunnerPlayerController.cs
+++ Assets/Scripts/RunnerPlayerController.cs
@@ -6,7 +6,14 @@ public class RunnerPlayerController : MonoBehaviour
 {
     public float PlayerSpeed;
 
+    public KeyCode BoostKey;
+    public KeyCode SwitchKey;
+
+    public GameObject W1;
+    public GameObject W2;
+
     private bool isAbleMove;
+    private bool isW1;
 
     private float boostTempo = 5f;
     private float nextBoost;
@@ -17,26 +24,42 @@ public class RunnerPlayerController : MonoBehaviour
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        isW1 = true;
+        W2.SetActive(false);
     }
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            isAbleMove = true;
-        }
-
         if (rb.velocity.magnitude < PlayerSpeed && isAbleMove)
         {
             rb.AddForce(Vector3.right * PlayerSpeed);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextBoost)
+        if (Input.GetKeyDown(BoostKey) && Time.time > nextBoost)
         {
             nextBoost = Time.time + boostTempo;
 
             rb.AddForce(Vector3.left * 3000f);
             rb.AddForce(Vector3.up * 1500f);
+        }
+
+        if (Input.GetKeyDown(SwitchKey))
+        {
+            if (isW1)
+            {
+                CameraController.Instance.SetCamBGColor();
+                W1.SetActive(false);
+                W2.SetActive(true);
+                isW1 = false;
+            }
+            else
+            {
+                CameraController.Instance.SetCamBGColor();
+                W1.SetActive(true);
+                W2.SetActive(false);
+                isW1 = true;
+            }
+
         }
     }
 
